714db131e08268aac69648e506c88e2e6ee33555
r-ajan0 <156338531+r-ajan0@users.noreply.github.com>
2026-02-11 22:46:34 +0545

    fix

--- src/model/Plan.java
+++ src/model/Plan.java
@@ -1,11 +1,11 @@
-package models;
+package model;
 
 import java.time.LocalDate;
 
-public abstract class plan {
+public abstract class Plan {
 
-    protected String PlanId;
-    protected String PlanName;
+    protected String planId;
+    protected String planName;
     protected String planType;
     protected double subscriptionRate;
     protected LocalDate creationDate;
