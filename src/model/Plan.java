a076a08ce5a4b1074f5a246b277949c7e5a8656f
atharva shrestha <atharvakop7@gmail.com>
2026-02-11 22:54:17 +0545

    constructor

--- src/model/Plan.java
+++ src/model/Plan.java
@@ -12,6 +12,16 @@ public abstract class Plan {
     protected int monthlyUsageLimit;
     protected boolean isActive;
 
+    public Plan(String planId, String planName, String planType, double subscriptionRate, LocalDate creationDate, int monthlyUsageLimit, boolean isActive) {
+        this.planId = planId;
+        this.planName = planName;
+        this.planType = planType;
+        this.subscriptionRate = subscriptionRate;
+        this.creationDate = creationDate;
+        this.monthlyUsageLimit = monthlyUsageLimit;
+        this.isActive = isActive;
+    }
+
     public abstract boolean canAddExtraPrompts();
     public abstract double calcExtraPromptCost(int numberOfPrompts);
     public abstract void displayPlanDetails();
